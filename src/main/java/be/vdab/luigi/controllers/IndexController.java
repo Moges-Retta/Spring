2f88cea7edd62c23686a7e858a5d646e14ad42c0
Moges <Moges.ash@gmail.com>
2020-07-17 20:29:06 +0200

    luigi webpage - controller

---
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package be.vdab.luigi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+
+@RestController
+@RequestMapping("/")
+class IndexController {
+    @GetMapping
+    public String index() {
+        var moment = LocalTime.now().getHour() < 12 ? "morgen" : "middag";
+        return "<!doctype html><html><title>Hallo</title><body>Goede "
+                + moment +
+                "</body></html>";
+    }
+}
+
