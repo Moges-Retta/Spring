86cc15383c4e13c227650848029fb93d26b30315
Moges <Moges.ash@gmail.com>
2020-07-18 17:26:15 +0200

    luigi webpage - thymeleaf - Getters

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -1,10 +1,13 @@
 package be.vdab.luigi.controllers;
 
+import be.vdab.luigi.domain.Adres;
+import be.vdab.luigi.domain.Persoon;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Controller
@@ -13,6 +16,11 @@ class IndexController {
     @GetMapping
     public ModelAndView index() {
         var morgenMiddag = LocalTime.now().getHour() < 12 ? "morgen" : "middag";
-        return new ModelAndView("index", "moment", morgenMiddag);    }
+        var modelAndView = new ModelAndView("index","moment", morgenMiddag);
+        modelAndView.addObject("zaakvoerder",
+        new Persoon("Luigi", "Peperone", 7, true, LocalDate.of(1966, 1, 31),
+        new Adres("Grote markt", "3", 9700, "Oudenaarde")));
+        return modelAndView;
+    }
 }
 
