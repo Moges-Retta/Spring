8df747d8b0f491ccb4b30ac22ff4aa2999cf9cf0
Moges <Moges.ash@gmail.com>
2020-07-18 15:58:45 +0200

    Frituur Frida thymeleaf

--- src/main/java/be/vdab/FrituurFrida/controllers/IndexController.java
+++ src/main/java/be/vdab/FrituurFrida/controllers/IndexController.java
@@ -1,21 +1,21 @@
 package be.vdab.FrituurFrida.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class IndexController {
     @GetMapping
-    public String index() {
-        var moment = LocalDate.now().getDayOfWeek()== DayOfWeek.MONDAY? "Gesloten" : "open";
-        return "<!doctype html><html><title>Hallo</title><body>Vandag is "
-                + moment +
-                "</body></html>";
+    public ModelAndView index() {
+        var welkdaag = LocalDate.now().getDayOfWeek()== DayOfWeek.MONDAY? "gesloten" : "open";
+        return new ModelAndView("index","moment",welkdaag);
     }
 }
