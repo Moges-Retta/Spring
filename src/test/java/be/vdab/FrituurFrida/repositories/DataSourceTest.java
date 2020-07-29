0aa791794a513e83f5a54f3cdc055771836a7900
Moges <Moges.ash@gmail.com>
2020-07-29 16:39:47 +0200

    EnterpriseApp - DatabaseTest

--- src/test/java/be/vdab/FrituurFrida/repositories/DataSourceTest.java
+++ src/test/java/be/vdab/FrituurFrida/repositories/DataSourceTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @JdbcTest
 public class DataSourceTest {
     private final DataSource dataSource;
@@ -14,7 +16,12 @@ public class DataSourceTest {
     }
     @Test
     void getConnection() throws SQLException {
-        try (var connection = dataSource.getConnection()) {
+        try (var connection = dataSource.getConnection();
+             var statement = connection.prepareStatement(
+                     "select count(*) as aantal from luigi.pizzas where pikant=1")) {
+             var result = statement.executeQuery();
+            result.next();
+            assertThat(result.getInt("aantal")).isEqualTo(1);
         }
     }
 }
